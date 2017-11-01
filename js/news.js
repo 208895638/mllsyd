f27eb2e99005002eceecfef7fb38820fabfa1ff8
yong zhu <208895638@qq.com>
2017-11-01 17:39:12 +0800

    11.1

--- js/news.js
+++ js/news.js
@@ -29,7 +29,29 @@ $(function(){
 			$(".back").on("click",function(){
 				window.history.go(-1);
 			});
-			
+			//返回顶部
+		    $('.go-back').on('click',function(){
+		        var top = $(window).scrollTop();
+		        var interval = setInterval(function(){
+		            top -= 50;
+		            if(top >= 50){
+		                $(window).scrollTop(top);
+		            }else {
+		                $(window).scrollTop(0);
+		                clearInterval(interval);
+		            }
+		        },20)
+		    });
+		     //底部按钮显示隐藏
+		    $(window).on('scroll',function(){
+		        var top = $(window).scrollTop();
+		        var height = document.body.offsetHeight;
+		        if(top >= height/2){
+		            $('.go-back').show();
+		        }else{
+		            $('.go-back').hide();
+		        }
+		    });
 		},
 		render:function(){
 			var that=this;
--- js/newsInfo.js
+++ js/newsInfo.js
@@ -21,7 +21,29 @@ $(function(){
 			$(".hbbs h3").on("click",function(){
 				$(".nav").slideUp();
 			});
-			
+			//返回顶部
+		    $('.go-back').on('click',function(){
+		        var top = $(window).scrollTop();
+		        var interval = setInterval(function(){
+		            top -= 50;
+		            if(top >= 50){
+		                $(window).scrollTop(top);
+		            }else {
+		                $(window).scrollTop(0);
+		                clearInterval(interval);
+		            }
+		        },20)
+		    });
+		     //底部按钮显示隐藏
+		    $(window).on('scroll',function(){
+		        var top = $(window).scrollTop();
+		        var height = document.body.offsetHeight;
+		        if(top >= height/2){
+		            $('.go-back').show();
+		        }else{
+		            $('.go-back').hide();
+		        }
+		    });
 		},
 		render:function(){
 			var that=this;
